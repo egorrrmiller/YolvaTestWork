8c2ee1f46b1230c2858574116df71a3c184cafeb
EgorkaMiller <ikuskov4@gmail.com>
2021-12-28 23:36:09 +0300

    поменял if на switch

--- YolvaTestWork/GeoServices/OSM/OpenStreetMap.cs
+++ YolvaTestWork/GeoServices/OSM/OpenStreetMap.cs
@@ -6,7 +6,8 @@ namespace YolvaTestWork.GeoServices.OSM;
 
 public class OpenStreetMap : IGeoService
 {
-    private readonly xNet.HttpRequest _httpRequest = new ();
+    private readonly xNet.HttpRequest _httpRequest = new();
+
     public Task<bool> CanExecute(GeoPolygonModel geoPolygonModel)
     {
         return Task.FromResult(geoPolygonModel.GeoService == GeoServicesEnum.OpenStreetMap);
@@ -14,45 +15,54 @@ public class OpenStreetMap : IGeoService
 
     public Task<string> Execute(GeoPolygonModel geoPolygonModel)
     {
-        _httpRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62";
-        var httpResponse = _httpRequest.Get($"https://nominatim.openstreetmap.org/search?q={geoPolygonModel.Address}&format=json&polygon_geojson=1").ToString();
-        
+        _httpRequest.UserAgent =
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62";
+        var httpResponse = _httpRequest
+            .Get($"https://nominatim.openstreetmap.org/search?q={geoPolygonModel.Address}&format=json&polygon_geojson=1")
+            .ToString();
+
         var json = JsonConvert.DeserializeObject<List<OpenStreetMapJsonModel>>(httpResponse)?.FirstOrDefault()?.Geojson;
         var typePolygonEnum = Enum.Parse<OsmTypePolygonEnum>(json!.Type);
 
-        object results = null!;
-        
-        if (typePolygonEnum == OsmTypePolygonEnum.Polygon)
-        {
-            List<object> polygon = new();
-            var coordinates =
-                JsonConvert.DeserializeObject<List<List<object>>>(json.Coordinates[0].ToString()!);
-            for (int i = 0; i < coordinates!.Count; i += geoPolygonModel.DotPolygon - 1)
-                polygon.Add(coordinates[i]);
+        object results;
 
-            results = polygon;
-        }
-        else if (typePolygonEnum == OsmTypePolygonEnum.MultiPolygon)
+        switch (typePolygonEnum)
         {
-            List<object> coords = new();
-            List<List<List<object>>> result = new();
-            foreach (var coordinate in json.Coordinates)
-            {
-                var coordinateJson = JsonConvert.DeserializeObject<List<List<List<object>>>>(coordinate.ToString()!);
-                foreach (var _ in coordinateJson!)
+            case OsmTypePolygonEnum.Polygon:
+                List<object> polygon = new();
+                var coordinates =
+                    JsonConvert.DeserializeObject<List<List<object>>>(json.Coordinates[0].ToString()!);
+                for (int i = 0; i < coordinates!.Count; i += geoPolygonModel.DotPolygon - 1)
+                    polygon.Add(coordinates[i]);
+
+                results = polygon;
+                break;
+
+            case OsmTypePolygonEnum.MultiPolygon:
+                List<object> coords = new();
+                List<List<List<object>>> result = new();
+                foreach (var coordinate in json.Coordinates)
                 {
-                    for (int i = 0; i < _.Count; i += geoPolygonModel.DotPolygon - 1)
-                        coords.Add(_[i]);
-                        
-                    result.Add(new List<List<object>> { coords });
+                    var coordinateJson =
+                        JsonConvert.DeserializeObject<List<List<List<object>>>>(coordinate.ToString()!);
+                    foreach (var _ in coordinateJson!)
+                    {
+                        for (int i = 0; i < _.Count; i += geoPolygonModel.DotPolygon - 1)
+                            coords.Add(_[i]);
 
-                    coords = new List<object>();
+                        result.Add(new List<List<object>> {coords});
+
+                        coords = new List<object>();
+                    }
                 }
-            }
 
-            results = result;
+                results = result;
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
         }
-        
+
         return Task.FromResult(JsonConvert.SerializeObject(results));
     }
 }
